f5e9b162ec3c030b36c178e5932e56e043fb425c
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-09-10 09:06:57 +0200

    Export the NODE-class

---
+++ src/Node.js
@@ -0,0 +1,7 @@
+export class NODE {
+  constructor(data) {
+    this.data = data;
+    this.left = null; // Reference to the left child node
+    this.right = null; // Reference to the right child node
+  }
+}
